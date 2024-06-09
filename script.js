2038d9ba250616407cb40ca96e5642701010e662
Nathan <nldblanch@gmail.com>
2024-06-09 21:56:07 +0100

    add basic structure for website

---
+++ script.js
@@ -0,0 +1,20 @@
+const navHome = document.getElementById("nav-home")
+const navAbout = document.getElementById("nav-about")
+const navProjects = document.getElementById("nav-projects")
+const navContact = document.getElementById("nav-contact")
+const projects = document.getElementById('projects')
+
+navHome.addEventListener("click", () => {
+    
+})
+
+navAbout.addEventListener("click", () => {
+   
+})
+
+navProjects.addEventListener("click", () => {
+    projects.classList.add("opened")  
+    
+})
+
+navContact.addEventListener("click", () => {})
