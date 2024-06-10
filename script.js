c4c65a269e432e6dc6b14b2a251e0d3313530f8e
Nathan <nldblanch@gmail.com>
2024-06-10 13:34:30 +0100

    add animations to show and hide scroll bar

--- script.js
+++ script.js
@@ -3,34 +3,32 @@ const navAbout = document.getElementById("nav-about")
 const navProjects = document.getElementById("nav-projects")
 const navContact = document.getElementById("nav-contact")
 const projects = document.querySelectorAll('.project')
+const projectContainer = document.getElementById('projectContainer')
 let projectsOpen = false;
 
+const closeProjects = () => {
+    projects.forEach((project) => {project.classList.replace("opened", "closed")})
+    projectContainer.classList.replace('showScroll', 'hideScroll')
+    projectsOpen = false;
+}
 navHome.addEventListener("click", () => {
     //if projects are open, close them
-    if (projectsOpen === true) {
-        projects.forEach((project) => {project.classList.replace("opened", "closed")})
-        projectsOpen = false;
-    }
+    if (projectsOpen === true) closeProjects()
 })
 
 navAbout.addEventListener("click", () => {
     //if projects are open, close them
-    if (projectsOpen === true) {
-        projects.forEach((project) => {project.classList.replace("opened", "closed")})
-        projectsOpen = false;
-    }
+    if (projectsOpen === true) closeProjects()
 
 })
 
 navProjects.addEventListener("click", () => {
     projects.forEach((project) => {project.classList.replace("closed", "opened")})
+    projectContainer.classList.replace('hideScroll', 'showScroll')
     projectsOpen = true;
 })
 
 navContact.addEventListener("click", () => {
       //if projects are open, close them
-      if (projectsOpen === true) {
-        projects.forEach((project) => {project.classList.replace("opened", "closed")})
-        projectsOpen = false;
-    }
+      if (projectsOpen === true) closeProjects()
 })
