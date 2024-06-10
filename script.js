b45350458c8cbdf0cc56081097e5f3f3bf5e261d
Nathan <nldblanch@gmail.com>
2024-06-10 19:15:29 +0100

    begun to work on nav replacers

--- script.js
+++ script.js
@@ -4,31 +4,59 @@ const navProjects = document.getElementById("nav-projects")
 const navContact = document.getElementById("nav-contact")
 const projects = document.querySelectorAll('.project')
 const projectContainer = document.getElementById('projectContainer')
+const about = document.getElementById('about')
+const aboutNavReplacer = document.getElementById('aboutNavReplacer')
+const projectsNavReplacer = document.getElementById('projectsNavReplacer')
+const contactNavReplacer = document.getElementById('contactNavReplacer')
+
+
 let projectsOpen = false;
+let aboutOpen = false;
+// let aboutNavReplacer = false;
+// let projectsNavReplacer = false;
+// let contactNavReplacer = false;
 
 const closeProjects = () => {
     projects.forEach((project) => {project.classList.replace("opened", "closed")})
     projectContainer.classList.replace('showScroll', 'hideScroll')
     projectsOpen = false;
 }
+const closeAbout = () => {
+    about.classList.replace('aboutOpen', 'aboutClose')
+    aboutOpen = false;
+}
+
 navHome.addEventListener("click", () => {
     //if projects are open, close them
     if (projectsOpen === true) closeProjects()
+    if (aboutOpen === true) closeAbout()
 })
 
+//WHEN YOU CLICK ABOUT
 navAbout.addEventListener("click", () => {
-    //if projects are open, close them
+    //close projects if open
     if (projectsOpen === true) closeProjects()
-
+    about.classList.remove('initiallyHidden')
+    about.classList.replace('aboutClose', 'aboutOpen')
+    aboutOpen = true;
+    //remove initial hidden div and replace class list
+    aboutNavReplacer.classList.remove('initiallyHidden')
+    navAbout.classList.replace('fadeIn', 'fadeOut')
+    aboutNavReplacer.classList.replace('fadeOut', 'fadeIn')
+    
 })
 
 navProjects.addEventListener("click", () => {
     projects.forEach((project) => {project.classList.replace("closed", "opened")})
     projectContainer.classList.replace('hideScroll', 'showScroll')
+    projectContainer.classList.remove('initiallyHidden')
+    if (aboutOpen === true) closeAbout()
     projectsOpen = true;
+
 })
 
 navContact.addEventListener("click", () => {
-      //if projects are open, close them
-      if (projectsOpen === true) closeProjects()
+    //if projects are open, close them
+    if (projectsOpen === true) closeProjects()
+    if (aboutOpen === true) closeAbout()
 })
